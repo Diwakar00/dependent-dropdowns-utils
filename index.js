44701d5bcd5f4a9b2b33b8318885368277d96cc6
Diwakar MK <doddacharsddiwakarmk@example.com>
2023-07-12 00:57:54 +0530

    commit to delete unwanted files

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-module.exports = {
-  getOptions: require("./getOptions"),
-  getAllPossibleOptions: require("./getAllPossibleOptions/getAllPossibleOptions"),
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getOptions = exports.getAllPossibleOptions = void 0;
+var getAllPossibleOptions_1 = require("./src/getAllPossibleOptions");
+Object.defineProperty(exports, "getAllPossibleOptions", { enumerable: true, get: function () { return getAllPossibleOptions_1.getAllPossibleOptions; } });
+var getOptions_1 = require("./src/getOptions");
+Object.defineProperty(exports, "getOptions", { enumerable: true, get: function () { return getOptions_1.getOptions; } });
+var a = 10;
+console.log(a);
